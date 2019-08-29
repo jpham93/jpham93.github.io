1433c24be863b76f4e2d69ca67d46c35fd2cb18c
James Pham <jamespham93@yahoo.com>
2019-08-29 06:14:01 -0700

    Fix grammar in Portfolio paragraph

--- src/pages/Projects/Portfolio.js
+++ src/pages/Projects/Portfolio.js
@@ -9,17 +9,21 @@ import {
 
 export default function () {
   return (
-    <Container>
+    <Container fluid>
       <Header inverted as='h3'>Portfolio Page</Header>
       <Grid stackable>
         <Grid.Row>
           <Grid.Column width={12}>
             <p>This entire web page is one of my projects itself! However, I
-            wanted to highlight it, because this is my first project where I
+            wanted to highlight it, because this is my first project outside of my internship where I
             applied proper Github workflow. Using knowledge from my VTA
             internship, I documentated the planning and execution through a
             series of Github issues, branches, and pull request.</p>
-            <p>This entire project runs on React with a React implemented Semantic UI. This project also uses React router to dynamically render React components web pages. This project is deployed in two places, Github Pages, and an AWS EC2 instance. A link of this website's source code can be found in the Github icon.</p>
+            <p>This entire project runs on React with a React implemented
+            Semantic UI. This project also uses React router to dynamically
+            render React components web pages. This project is deployed in two
+            places, Github Pages, and an AWS EC2 instance. A link of this
+            website's source code can be found in the Github icon.</p>
           </Grid.Column>
           <Grid.Column width={4}>
             <Header inverted as='h5'>Tech:</Header>
