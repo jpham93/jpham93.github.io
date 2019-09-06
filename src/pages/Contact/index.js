2c657ecbe371ed8752288dd6792eaf49c552878f
James Pham <jamespham93@yahoo.com>
2019-09-06 10:30:06 -0700

    Add Contact page instructions

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import {
+  Container
+} from 'semantic-ui-react';
 
 export default function () {
   return (
-    <div>Contact</div>
+    <Container fluid>
+      <Container>
+        <p>
+          If you would like to contact me, my email is provided in header above the menu. You can also email using the form below if you prefer to do it from this web page. If you would like to set up a phone call or Video chat, please email me first with your contact information and questions first. Thank you!
+        </p>
+      </Container>
+    </Container>
   );
 }
