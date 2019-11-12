1db6f427a69391de56be24d59ac50ed4f5ea918e
James Pham <jamespham93@yahoo.com>
2019-11-11 19:39:49 -0800

    Implemented css transition between switching route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import 'semantic-ui-css/semantic.min.css';
 import './App.css';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, withRouter } from 'react-router-dom';
 import { CircleArrow as ScrollUpButton } from 'react-scroll-up-button';
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 import {
   Footer,
   Navbar,
@@ -34,19 +35,37 @@ const scrollUpBtnStyle = {
   zIndex: '99',
 };
 
+function MainWrapper({location}) {
+  return (
+    <div style={mainStyle} classNames='fade-enter fade-enter-active fade-exit fade-exit-active'>
+      <TransitionGroup>
+        <CSSTransition
+          key={location.key}
+          timeout={{enter: 700, exit: 700}}
+          classNames='fade'
+        >
+          <Switch>
+            <Route exact path='/projects' component={Projects}/>
+            <Route exact path='/about' component={About}/>
+            <Route exact path='/contact' component={Contact}/>
+            <Route exact path='/' component={Resume}/>
+            <Route path='*' component={NotFound}/>
+          </Switch>
+        </CSSTransition>
+      </TransitionGroup>
+    </div>
+  );
+}
+
+const Main = withRouter(MainWrapper);
+
 function App() {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <div style={mainStyle}>
         <Navbar/>
+        <Main/>
         <ScrollUpButton style={scrollUpBtnStyle}/>
-        <Switch>
-          <Route exact path='/projects' component={Projects}/>
-          <Route exact path='/about' component={About}/>
-          <Route exact path='/contact' component={Contact}/>
-          <Route exact path='/' component={Resume}/>
-          <Route path='*' component={NotFound}/>
-        </Switch>
         <Footer attached='bottom'/>
       </div>
     </BrowserRouter>
