9ed71f9ef9003f3db54ca9f082afb13bd8920728
James Pham <jamespham93@yahoo.com>
2019-11-13 17:15:13 -0800

    Added placeholder for loading heashot in Resume

--- src/pages/Resume/Introduction.js
+++ src/pages/Resume/Introduction.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState, useLayoutEffect } from 'react';
 import {
   Container,
   Header,
   Divider,
   Button,
   Responsive,
+  Placeholder
 } from 'semantic-ui-react';
 import headshot from '../../assets/images/headshot.png';
 import './resume.css';
@@ -38,7 +39,20 @@ const imgStyle = {
   flexShrink: '0',
 };
 
+/**
+ * PlacerHolder Style
+ *
+ * Sets dimensions for headshot placeholder
+ */
+const placeHolderStyle = {
+  width: 200,
+  height: 200,
+  borderRadius: '50%'
+};
+
 export default function () {
+  const [imageLoaded, setLoaded] = useState(false);
+
   const getResume = () => {
     window.open('Resume_JP.pdf');
   };
@@ -47,12 +61,20 @@ export default function () {
     window.open('https://linkedin.com/in/james-pham-0293')
   };
 
+  useLayoutEffect(() => {
+    const image = new Image();
+    image.src = headshot;
+    image.onload = () => setLoaded(true);
+  }, [imageLoaded]);
+
   return (
     <Container style={introContainer}>
+
       <Container id='intro-text-container'>
         <Header as='h1' style={{fontSize: '0.5em', lineHeight: '20%'}}>Hi!</Header>
-        <Header as='h2' style={{fontSize: '0.6em', marginTop: '0'}}>My Name is <span
-          style={{fontSize: '1.35em'}}>James Pham</span></Header>
+        <Header as='h2' style={{fontSize: '0.6em', marginTop: '0'}}>
+          My Name is <span style={{fontSize: '1.35em'}}>James Pham</span>
+        </Header>
         <Divider/>
         <Header as='h3'>I am a Junior Front-End Web Developer.</Header>
         <div className={'button-wrapper'}>
@@ -62,9 +84,19 @@ export default function () {
           <Button id='linkedin-btn' className={'button-style'} onClick={openLinkedin}>Linkedin</Button>
         </div>
       </Container>
+
       <Responsive minWidth={768} style={{marginRight: 'auto'}}>
-        <div style={imgStyle}/>
+        {
+          imageLoaded
+            ?
+            <div style={imgStyle}/>
+            :
+            <Placeholder style={placeHolderStyle}>
+              <Placeholder.Image/>
+            </Placeholder>
+        }
       </Responsive>
+
     </Container>
   );
 }
